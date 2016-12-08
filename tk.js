861d18b2d6d76c120eef23969e6c407761f46c39
dongming lu <dongming lu>
2016-12-08 16:00:35 +0800

    changes log

--- tk.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	(function(){
-$('#main_menu li').hover(function() { 
-$(this).find('.sub-menu').css('display', 'block'); 
-}, function() { 
-$(this).find('.sub-menu').css('display', 'none'); 
-});  
-	})();
-});
